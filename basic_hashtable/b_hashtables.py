b04b36c5a6689bdba88ee1cea7e5e1f4bfcc6e5d
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-07-09 13:35:22 -0400

    From class: Make slight changes to remove/retrieve, add notes

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -43,7 +43,7 @@ def hash_table_insert(hash_table, key, value):
     index = hash(key, hash_table.capacity)
     print("INDEX IN INSERT", index)
 
-    # create new Pair from key and value -- not sure how to utilize this properly?
+    # create new Pair from key and value
     pair = Pair(key, value)
     print("PAIR", pair)
 
@@ -57,7 +57,7 @@ def hash_table_insert(hash_table, key, value):
             print("WARNING: Value will be overridden!")
 
     # insert value at given index, even if existing value is not None
-    hash_table.storage[pair.key] = pair
+    hash_table.storage[index] = pair
 
     # printing just to see process when running tests
     print("STORAGE IN INSERT")
@@ -77,7 +77,7 @@ def hash_table_remove(hash_table, key):
     print("INDEX IN REMOVE", index)
 
     # if the value at the given index is none print warning
-    if hash_table.storage[index] is None:
+    if hash_table.storage[index] is None or hash_table.storage[index].key is not key:
         print("WARNING: No value to remove!")
     # else set the value to None
     else:
@@ -101,10 +101,13 @@ def hash_table_retrieve(hash_table, key):
     
     # if the value at that index is None return None
     if hash_table.storage[index] is None:
+        # print a warning
+        print("Unable to find value with key " + key)
         return None
-    # else return the value
+    # else, check if the stored index's key equals the key, and return the value
     else:
-        return hash_table.storage[index]
+        if hash_table.storage[index].key == key:
+            return hash_table.storage[index].value
 
 
 def Testing():
